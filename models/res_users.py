aee2e6a8002f1d2eb0971f135b7f77f8498b9188
Bishal Pun <bishalpun2013@gmail.com>
2018-05-25 12:52:07 +0545

    Extend res users model

---
+++ models/res_users.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import uuid
+from odoo import models, fields
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    oauth_identifier = fields.Char(
+        string='OAuth Identifier', required=True, readonly=True,
+        default=lambda self: str(uuid.uuid4()), copy=False,
+        help='String used to identify this user during an OAuth session.')
+
+    _sql_constraints = [
+        ('oauth_identifier_unique', 'UNIQUE (oauth_identifier)',
+         'The OAuth identifier of the user must be unique !'),
+    ]
