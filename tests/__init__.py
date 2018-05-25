fa2de227ccbd7642319922c419493027bd6f1d8b
Bishal Pun <bishalpun2013@gmail.com>
2018-05-25 13:31:10 +0545

    Create unit tests for python side

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from . import test_oauth_provider_client
+from . import test_oauth_provider_token
+from . import test_oauth_provider_scope
+from . import test_oauth_provider_controller_web_application
+from . import test_oauth_provider_controller_mobile_application
+from . import test_oauth_provider_controller_legacy_application
