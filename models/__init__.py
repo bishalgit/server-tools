e4c6f6881fe3beb4101cb682b2c5ddab51a751b9
Bishal Pun <bishalpun2013@gmail.com>
2018-05-25 13:28:46 +0545

    Import models

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from . import models
+from . import oauth_provider_authorization_code
+from . import oauth_provider_redirect_uri
+from . import oauth_provider_scope
+from . import oauth_provider_token
+from . import oauth_provider_client
+from . import res_users
