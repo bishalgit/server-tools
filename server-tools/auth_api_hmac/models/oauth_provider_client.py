becea4fe13ae2b4d4a0551399044b0eb2c7c1afa
Bishal Pun <bishalpun2013@gmail.com>
2018-06-15 20:16:10 +0545

    [ADD] auth_api_hmac: add api_key and method to generate it

--- server-tools/auth_api_hmac/models/oauth_provider_client.py
+++ server-tools/auth_api_hmac/models/oauth_provider_client.py
@@ -22,6 +22,15 @@ class OAuthProviderClient(models.Model):
         string='API Signature', compute='_compute_api_signature',
         help='Signature used for the API request validation.')
 
+    api_key = fields.Text(
+        string='API Key', help='API key used for signing API requests.')
+
+    api_key_algorithm = fields.Selection(selection=[
+        ('HS256', 'HMAC using SHA-256 hash algorithm'),
+        ('HS384', 'HMAC using SHA-384 hash algorithm'),
+        ('HS512', 'HMAC using SHA-512 hash algorithm'),
+    ], string='API signing Algorithm', help='Algorithm used to sign the API requests.')
+
     @api.multi
     def _compute_api_signature(self):
         """ Compute the api signature associated to the client's private key
@@ -29,10 +38,30 @@ class OAuthProviderClient(models.Model):
         This is only done for symmetric algorithms
         """
         for client in self:
-            if client.jwt_private_key and \
-                    client.jwt_algorithm[:2] == 'HS':
-                h = hmac.HMAC(bytes(client.jwt_private_key, 'utf-8'), hashes.SHA256(), backend=default_backend())
+            if client.api_key and \
+                    client.api_key_algorithm[:2] == 'HS':
+                h = hmac.HMAC(bytes(client.api_key, 'utf-8'), hashes.SHA256(), backend=default_backend())
                 h.update(bytes(client.identifier, 'utf-8'))
                 client.api_signature = h.finalize()
             else:
                 client.api_signature = False
+
+    @api.multi
+    def generate_api_key(self):
+        """ Generate a private hmac key for HMAC algorithm clients """
+        for client in self:
+            algorithm_prefix = client.api_key_algorithm[:2]
+            algorithm_suffix = client.api_key_algorithm[2:]
+
+            if algorithm_prefix == 'HS':
+                if algorithm_suffix == '256':
+                    key = hashlib.sha256(os.urandom(32)).hexdigest()
+                elif algorithm_suffix == '384':
+                    key = hashlib.sha384(os.urandom(48)).hexdigest()
+                elif algorithm_suffix == '512':
+                    key = hashlib.sha512(os.urandom(64)).hexdigest()
+            else:
+                raise exceptions.UserError(
+                    _('You can only generate private hmac keys for symetric '
+                      'algorithms!'))
+            client.api_key = key
